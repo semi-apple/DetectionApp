f2799c248f65451873d25588a8b583cfe3c78609
Kun Zhou <kun.zhou@uq.net.au>
2024-11-18 14:14:02 +1000

    update install.bat

--- install.bat
+++ install.bat
@@ -1,17 +1,40 @@
-REM Activate venv vWindwos
-python -m venv venv
-./venv/Scripts/activate
+@echo off
+python --version >null 2>&1
+if %errorlevel% neq 0 (
+    echo "Python is not installed, please install Python 3.11+ and try again."
+    exit /b 1
+    )
 
-REM Install requirements from requirements.txt
-pip install setuptools
-pip cache purge
+echo "Creating virtual environment..."
+python -m venv venv
+call /venv/Scripts/activate
+
+echo "Updating pip..."
+python -m pip install --upgrade pip
+
+echo "Installing dependencies..."
+@REM pip install setuptools
+@REM pip cache purge
 pip install -r requirements.txt
 
-REM Uninstall torch, torchvision, torchaudio
+@REM REM Uninstall torch, torchvision, torchaudio
 pip uninstall torch torchvision torchaudio -y
 
-REM Purge pip cache
+@REM REM Purge pip cache
 pip cache purge
 
-REM Install specific versions of torch, torchvision, torchaudio
+@REM REM Install specific versions of torch, torchvision, torchaudio
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
+
+echo "Installation completed!"
+echo "Packaging application..."
+pyinstaller --onefile app\main.py
+
+if exist dist\main.exe (
+    echo "Build successful!"
+    exit /b 0
+) else (
+    echo "Build failed: No executable found."
+    exit /b 1
+)
+@REM pause
