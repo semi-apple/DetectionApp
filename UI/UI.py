fe3e93e9c323dd502161367dc1f56834028be78f
Kun Zhou <kun.zhou@uq.net.au>
2024-11-28 21:02:22 +1000

    update

--- UI/UI.py
+++ UI/UI.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'draft.ui'
+# Form implementation generated from reading ui file 'interface.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.10
 #
